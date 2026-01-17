b436e0a7aabb16d35081ac4d1154b0849ba9009a
RobsonTrasel <robson.trasel@gmail.br>
2026-01-17 16:48:19 -0300

    feat(core): add core source structure
      - Create main index.ts with public API exports
      - Setup directory structure for Model, Builder, Database, Schema
      - Prepare directories for relations, contracts, and support utilities

---
+++ packages/core/src/index.ts
@@ -0,0 +1,7 @@
+// Velvet ORM - Core Package
+// Export all public APIs
+
+export { Model } from './Model'
+export { Builder } from './Builder'
+export { Database } from './Database'
+export { Schema } from './Schema'
