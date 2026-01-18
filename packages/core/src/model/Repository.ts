0cea1fde4ca2e258f948c6e309e5d29cc7b54343
RobsonTrasel <robson.trasel@gmail.br>
2026-01-18 00:42:11 -0300

    feat(model): create a optional repository wrapper for SRP and DX

---
+++ packages/core/src/model/Repository.ts
@@ -0,0 +1,100 @@
+/**
+ * Repository
+ *
+ * Optional repository wrapper that keeps query logic outside Model.
+ * Provides a familiar API without forcing users away from User.query().
+ */
+
+import type { Attributes } from '../contracts/ModelContract'
+import type { ComparisonOperator } from '../types'
+import { QueryProxy } from './QueryProxy'
+import type { Model, ModelConstructor } from '../Model'
+
+export class Repository<T extends Model = Model> {
+  private readonly proxy: QueryProxy<T>
+
+  constructor(modelClass: ModelConstructor<T>) {
+    this.proxy = new QueryProxy<T>(modelClass)
+  }
+
+  query() {
+    return this.proxy.query()
+  }
+
+  where(column: string, value: unknown): ReturnType<QueryProxy<T>['where']>
+  where(column: string, operator: ComparisonOperator, value: unknown): ReturnType<QueryProxy<T>['where']>
+  where(
+    column: string,
+    operatorOrValue: ComparisonOperator | unknown,
+    value?: unknown
+  ): ReturnType<QueryProxy<T>['where']> {
+    if (arguments.length === 2) {
+      return this.proxy.where(column, operatorOrValue)
+    }
+    return this.proxy.where(column, operatorOrValue as ComparisonOperator, value)
+  }
+
+  find(id: string | number) {
+    return this.proxy.find(id)
+  }
+
+  findOrFail(id: string | number) {
+    return this.proxy.findOrFail(id)
+  }
+
+  first() {
+    return this.proxy.first()
+  }
+
+  all() {
+    return this.proxy.all()
+  }
+
+  get() {
+    return this.proxy.get()
+  }
+
+  create(attributes: Partial<Attributes>) {
+    return this.proxy.create(attributes)
+  }
+
+  with(...relations: string[]) {
+    return this.proxy.with(...relations)
+  }
+
+  orderBy(column: string, direction?: 'asc' | 'desc') {
+    return this.proxy.orderBy(column, direction)
+  }
+
+  latest(column?: string) {
+    return this.proxy.latest(column)
+  }
+
+  oldest(column?: string) {
+    return this.proxy.oldest(column)
+  }
+
+  limit(value: number) {
+    return this.proxy.limit(value)
+  }
+
+  take(value: number) {
+    return this.proxy.take(value)
+  }
+
+  count() {
+    return this.proxy.count()
+  }
+
+  withTrashed() {
+    return this.proxy.withTrashed()
+  }
+
+  onlyTrashed() {
+    return this.proxy.onlyTrashed()
+  }
+
+  scope(name: string, ...args: unknown[]) {
+    return this.proxy.scope(name, ...args)
+  }
+}
