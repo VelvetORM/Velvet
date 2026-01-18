a984c1523f544434650ef91d301830f6dfed1046
RobsonTrasel <robson.trasel@gmail.br>
2026-01-18 00:37:26 -0300

    chore(relations): create a index export for relations

---
+++ packages/core/src/relations/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Relations Module
+ *
+ * Exports all relation types for use in models.
+ */
+
+export { Relation } from './Relation'
+export { HasOne } from './HasOne'
+export { HasMany } from './HasMany'
+export { BelongsTo } from './BelongsTo'
+export { BelongsToMany } from './BelongsToMany'
