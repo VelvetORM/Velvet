f2800e576efb4bd50dbb0661db3e384cc7f9458b
RobsonTrasel <robson.trasel@gmail.br>
2026-01-18 00:41:33 -0300

    chore(model): add imports to index

---
+++ packages/core/src/model/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Model Components
+ *
+ * Extracted components that compose a Model.
+ */
+
+export { AttributeBag } from './AttributeBag'
+export type { CastMap, MutatorRegistry, AccessorRegistry, AttributeBagConfig } from './AttributeBag'
+
+export { ModelSerializer } from './ModelSerializer'
+export type { Serializable, SerializationOptions } from './ModelSerializer'
+
+export { ModelEventDispatcher, ModelEvents } from './ModelEventDispatcher'
+export type { EventPriority, EventDispatchResult } from './ModelEventDispatcher'
+
+export { ModelPersister } from './ModelPersister'
+export { ModelValidator } from './ModelValidator'
+export { QueryProxy } from './QueryProxy'
+export { Repository } from './Repository'
+export { ModelHydrator } from './ModelHydrator'
+export { RelationLoader } from './RelationLoader'
